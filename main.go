4b85a6c445545a527d860a1a8fa33e4186ecc6c4
sivaplaysmC <sivaram.asdf@gmail.com>
2024-03-15 18:51:17 +0530

    add shit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/elazarl/goproxy"
+)
+
+type myProxyWrapper struct {
+	proxy http.Handler
+}
+
+func main() {
+	proxy := goproxy.NewProxyHttpServer()
+	proxy.Verbose = false
+	proxy.OnResponse().DoFunc(
+		func(r *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
+			r.Header.Set("X-Proxy", "fvin_vf_pbby")
+			log.Println(r.Request.Method, r.Request.URL)
+			return r
+		})
+
+	address := ":" + os.Args[1]
+	fmt.Println("starting http proxy at ", address)
+	log.Fatal(http.ListenAndServe(address, proxy))
+}
